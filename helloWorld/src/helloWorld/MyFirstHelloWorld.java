b80f938a8122d192c714b455bc7768ca758003d6
Kunal Das <kunal.b.das@capgemini.com>
2017-11-14 12:32:12 +0530

    Signed-off-by: Kunal Das <kunal.b.das@capgemini.com>

---
+++ helloWorld/src/helloWorld/MyFirstHelloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class MyFirstHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Program has run succesfully");
+		System.out.println("Hello Woooorld!!!");
+		System.out.println("nog een derde regel");
+	}
+
+}
